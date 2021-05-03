7366aedd9b7f539d8b2e4ad842fd99e05bc68738
Alex Standiford <a@alexstandiford.com>
2021-05-03 16:28:21 -0700

    Fixes issues where extended plugins did not get registered items.

--- roles.php
+++ roles.php
@@ -4,13 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $class ) {
-	if ( 'Underpin\Underpin' === $class ) {
-		require_once( plugin_dir_path( __FILE__ ) . 'Role.php' );
-		require_once( plugin_dir_path( __FILE__ ) . 'Role_Instance.php' );
-		Underpin\underpin()->loaders()->add( 'roles', [
-			'instance' => 'Underpin_Roles\Abstracts\Role',
-			'default'  => 'Underpin_Roles\Factories\Role_Instance',
-		] );
-	}
+add_action( 'underpin/before_setup', function ( $instance ) {
+	require_once( plugin_dir_path( __FILE__ ) . 'Role.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'Role_Instance.php' );
+	$instance->loaders()->add( 'roles', [
+		'instance' => 'Underpin_Roles\Abstracts\Role',
+		'default'  => 'Underpin_Roles\Factories\Role_Instance',
+	] );
 } );
