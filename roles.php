45c598a00b1a2b68be52686038afff8fdf439172
Alex Standiford <a@alexstandiford.com>
2021-05-05 15:52:58 -0700

    Corrects underpin autoloader

--- roles.php
+++ roles.php
@@ -4,10 +4,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $instance ) {
+add_action( 'underpin/before_setup', function ( $file ) {
 	require_once( plugin_dir_path( __FILE__ ) . 'Role.php' );
 	require_once( plugin_dir_path( __FILE__ ) . 'Role_Instance.php' );
-	$instance->loaders()->add( 'roles', [
+	Underpin\underpin()->get( $file )->loaders()->add( 'roles', [
 		'instance' => 'Underpin_Roles\Abstracts\Role',
 		'default'  => 'Underpin_Roles\Factories\Role_Instance',
 	] );
