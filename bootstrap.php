fc88fe1171daf51f4582452bf4fc46ef87820808
Alex Standiford <a@alexstandiford.com>
2021-11-24 15:41:36 -0600

    Fixes loader

--- bootstrap.php
+++ bootstrap.php
@@ -10,6 +10,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 Underpin::attach( 'setup', new \Underpin\Factories\Observers\Loader( 'roles', [
 	'name'        => 'Roles',
 	'description' => 'Handles setting custom roles in WordPress',
-	'instance'    => 'Underpin\Roles\Abstracts\Role',
-	'default'     => 'Underpin\Roles\Factories\Role_Instance',
+	'abstraction_class'    => 'Underpin\Roles\Abstracts\Role',
+	'default_factory'     => 'Underpin\Roles\Factories\Role_Instance',
 ] ) );
